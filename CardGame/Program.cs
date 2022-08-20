854f023df422703c6a65b87683b6ac1bc701a738
Sam <snippetsbysam@outlook.com>
2022-08-20 16:54:09 +0100

    deal cards

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -1,4 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CardGame;
+
+// Poker
+PokerDeck pokerDeck = new PokerDeck();
+
+var hand = pokerDeck.DealCards();
+Console.WriteLine("Poker:");
+foreach (var card in hand)
+{
+    Console.WriteLine($"{card.Value.ToString()} of {card.Suit.ToString()}");
+}
+
+// BlackJack
+BlackJackDeck blackJackDeck = new BlackJackDeck();
+
+hand = blackJackDeck.DealCards();
+Console.WriteLine();
+Console.WriteLine("BlackJack:");
+foreach (var card in hand)
+{
+    Console.WriteLine($"{card.Value.ToString()} of {card.Suit.ToString()}");
+}
+
 Console.ReadLine();
 
 
