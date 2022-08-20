82fcd7635477352554f98e72363eb641aa1c0864
Sam <snippetsbysam@outlook.com>
2022-08-20 15:15:00 +0100

    complete createDeck

--- CardGame/Models/Deck.cs
+++ CardGame/Models/Deck.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace CardGame.Models
 {
-    public class Deck
+    public abstract class Deck
     {
+        protected List<PlayingCard> fullDeck = new List<PlayingCard>();
+        protected List<PlayingCard> drawPile = new List<PlayingCard>();
+        protected List<PlayingCard> discardPile = new List<PlayingCard>();
+       
+        public void CreateDeck()
+        {
+            fullDeck.Clear();
+            foreach (var suit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (var value in Enum.GetValues(typeof(CardValue)))
+                {
+                    fullDeck.Add(new PlayingCard { Suit = (CardSuit)suit, Value = (CardValue)value});
+                }
+            }
+        }
+
+        public virtual void ShuffleDeck()
+        {
+
+        }
+
+        public abstract List<PlayingCard> DealCard();
+
+        public virtual PlayingCard RequestCard()
+        {
+
+        }
     }
 }
