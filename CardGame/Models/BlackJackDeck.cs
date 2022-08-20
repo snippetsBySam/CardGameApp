bf3fa97a79b7761201c81618f9f1ba36a013d880
Sam <snippetsbysam@outlook.com>
2022-08-20 16:11:43 +0100

    add BlackJackDeck

---
+++ CardGame/Models/BlackJackDeck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Models
+{
+    public class BlackJackDeck : Deck
+    {
+        public override List<PlayingCard> DealCards()
+        {
+            List<PlayingCard> output = new List<PlayingCard>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                output.Add(DrawOneCard());
+            }
+            return output;
+        }
+    }
+}
